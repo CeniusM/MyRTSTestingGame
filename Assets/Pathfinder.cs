fc21ef984aeb230f1e448de328393a91c9747f28
CeniusM <cenius.meier@gmail.com>
2025-10-27 17:01:56 +0100

    Just backingup after working on tiles and pathfiner

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,409 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using NodeIndex = System.Int32;
+
+namespace AStarPathfindingMaze
+{
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public struct Node
+    {
+        public bool DONE;
+        public int HeapIndex;
+
+        /// <summary>
+        /// Distance from start
+        /// </summary>
+        public int GCost;
+
+        /// <summary>
+        /// Distance to end
+        /// </summary>
+        public int HCost;
+
+        /// <summary>
+        /// Total cost
+        /// </summary>
+        public int FCost;
+
+        /// <summary>
+        /// Index to parrent node
+        /// </summary>
+        public NodeIndex Parent;
+
+        public Node(int gCost, int hCost, NodeIndex parent)
+        {
+            DONE = false;
+            HeapIndex = 0;
+
+            GCost = gCost;
+            HCost = hCost;
+            FCost = gCost + hCost;
+            Parent = parent;
+        }
+
+        public override string ToString()
+        {
+            return "N:" + FCost;
+        }
+    }
+
+    public class NodeHeap
+    {
+        // Instead, we just index with int into 1d node array
+        // That way we have less heap, no update, and can get both FCost, GCost and HCost
+        PathFinder _parrent;
+        private string[] DebugHeap => Heap.Select(x => _parrent._nodes[x].ToString()!).ToArray();
+        public readonly NodeIndex[] Heap;
+        public readonly int Size;
+        public int Count;
+
+        public NodeHeap(PathFinder parrent, int size)
+        {
+            _parrent = parrent;
+            Heap = new NodeIndex[size];
+            Size = size;
+            Count = 0;
+        }
+
+        public void Add(NodeIndex element)
+        {
+            if (Count == Size)
+                throw new Exception();
+
+            int index = Count;
+            _parrent._nodes[element].HeapIndex = index;
+            Heap[Count++] = element;
+            SortUp(index);
+        }
+
+        public NodeIndex Pop()
+        {
+            if (Count == 0)
+                throw new Exception();
+
+            NodeIndex first = Heap[0];
+            Count--;
+
+            _parrent._nodes[Heap[Count]].HeapIndex = 0;
+            Heap[0] = Heap[Count];
+
+            SortDown(0);
+
+            return first;
+        }
+
+        private void SortDown(int index)
+        {
+            while (true)
+            {
+                int leftChildIndex = LeftChild(index);
+                int rightChildIndex = RightChild(index);
+
+                if (leftChildIndex < Count)
+                {
+                    int swapIndex = leftChildIndex;
+
+                    if (rightChildIndex < Count)
+                    {
+                        if (_parrent._nodes[Heap[leftChildIndex]].FCost > _parrent._nodes[Heap[rightChildIndex]].FCost)
+                        {
+                            swapIndex = rightChildIndex;
+                        }
+                    }
+
+                    if (_parrent._nodes[Heap[index]].FCost > _parrent._nodes[Heap[swapIndex]].FCost)
+                    {
+                        Swap(index, swapIndex);
+                        index = swapIndex;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void SortUp(int index)
+        {
+            while (index != 0)
+            {
+                int parentIndex = Parent(index);
+
+                int parrentCost = _parrent._nodes[Heap[parentIndex]].FCost;
+
+                if (parrentCost > _parrent._nodes[Heap[index]].FCost)
+                {
+                    Swap(index, parentIndex);
+                    index = parentIndex;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Swap(int first, int second)
+        {
+            _parrent._nodes[Heap[first]].HeapIndex = second;
+            _parrent._nodes[Heap[second]].HeapIndex = first;
+
+            NodeIndex temp = Heap[first];
+            Heap[first] = Heap[second];
+            Heap[second] = temp;
+        }
+
+        private int LeftChild(int index)
+        {
+            return index * 2 + 1;
+        }
+
+        private int RightChild(int index)
+        {
+            return index * 2 + 2;
+        }
+
+        private int Parent(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        internal void SortUpNode(NodeIndex node)
+        {
+            SortUp(_parrent._nodes[node].HeapIndex);
+        }
+
+        internal void Clear()
+        {
+            Count = 0;
+        }
+    }
+
+    // Should be remade to handle float positions and negative coordinates, so map from -50,-50 to 50,50 is possible
+    public class PathFinder
+    {
+        public readonly int Width;
+        public readonly int Height;
+
+        // Maybe switch out with int array, then have something like int.maxvalue as a wall
+        // then we can use the rest of the values a move penelty
+        // Can also for fun test if it makes a diffrence if we parse multiple units,
+        // and increase the map value for where they wanna walk so they walk a bit diffrent paths
+        // For when a unit is 2 wide, we can make a temp map and make all 1 gaps solid for that search #_# => ### and #__# => #__#
+        public readonly bool[,] Map;
+
+        private NodeIndex _end;
+        private int _endX;
+        private int _endY;
+        private int _startX;
+        private int _startY;
+        internal readonly Node[] _nodes;
+        private readonly NodeHeap _heap;
+
+        const int DiagonalCost = 14;
+        const int StraightCost = 10;
+
+        public PathFinder(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Map = new bool[height, width];
+            _nodes = new Node[height * width];
+            _heap = new NodeHeap(this, Width * Height);
+        }
+
+        public void DebugCreateHCosts(Coord end)
+        {
+            Array.Clear(_nodes, 0, _nodes.Length);
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    _nodes[y * Width + x].HCost = Distance(x, y, end.x, end.y);
+                }
+            }
+        }
+
+        public List<Coord> SearchPath(Coord start, Coord end, bool doSmoothingParse = false)
+        {
+            // ThrowIf()
+            if (IsOutside(start.x, start.y) || IsOutside(end.x, end.y))
+            {
+                Debug.LogError("Start or end is outside of map\n" +
+                    $"start: {start.x}:{start.y}\n" +
+                    $"end: {end.x}:{end.y}\n" +
+                    $"size: {Width}:{Height}");
+                return new List<Coord>();
+            }
+
+            _end = end.y * Width + end.x;
+            _endX = end.x;
+            _endY = end.y;
+            _startX = start.x;
+            _startY = start.y;
+
+            Array.Clear(_nodes, 0, _nodes.Length);
+
+            _heap.Clear();
+            _heap.Add(start.y * Width + start.x);
+
+            NodeIndex lastPoint = start.y * Width + start.x;
+            NodeIndex squareLookedAt;
+            while ((squareLookedAt = Step()) != _end && squareLookedAt != int.MaxValue)
+            {
+                lastPoint = squareLookedAt;
+            }
+
+            // Still return list to the node that got the clostest to end
+            bool failed = squareLookedAt == int.MaxValue;
+            if (!failed)
+            {
+                lastPoint = squareLookedAt;
+            }
+
+            var path= GetSearchedPath(start.y * Width + start.x, lastPoint);
+
+            if (doSmoothingParse)
+                return SmoothPath(path);
+            else
+                return path;
+        }
+
+        // Will first remove all coords that is just in a straight line
+        // Then it will try to cut corners by checking if a straight line between two points is walkable
+        private List<Coord> SmoothPath(List<Coord> path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private NodeIndex Step()
+        {
+            if (_heap.Count == 0)
+                return int.MaxValue;
+
+            NodeIndex nodeIndex = _heap.Pop();
+            _nodes[nodeIndex].DONE = true;
+            int gCost = _nodes[nodeIndex].GCost;
+
+            int nodeX = nodeIndex % Width;
+            int nodeY = nodeIndex / Width;
+
+            for (int yOffset = -1; yOffset <= 1; yOffset++)
+            {
+                for (int xOffset = -1; xOffset <= 1; xOffset++)
+                {
+                    if (yOffset == 0 && xOffset == 0)
+                        continue;
+
+                    int x = xOffset + nodeX;
+                    int y = yOffset + nodeY;
+
+                    NodeIndex nextIndex = y * Width + x;
+
+                    if (IsOutside(x, y))
+                        continue;
+                    if (Map[y, x])
+                        continue;
+                    if (_nodes[nextIndex].DONE)
+                        continue;
+
+                    bool isDiagonal = xOffset != 0 && yOffset != 0;
+
+                    int moveCost = isDiagonal ? DiagonalCost : StraightCost;
+
+                    Node nextNode = _nodes[nextIndex];
+
+                    bool hasBeenLookedAt = nextNode.FCost != 0;
+
+                    Node newNode = new Node(
+                        gCost + moveCost,
+                        hasBeenLookedAt ?
+                        nextNode.HCost : Distance(x, y, _endX, _endY),
+                        nodeIndex
+                        );
+
+                    if (!hasBeenLookedAt ||
+                       nextNode.FCost > newNode.FCost)
+                    {
+                        int indexSave = _nodes[nextIndex].HeapIndex;
+                        _nodes[nextIndex] = newNode;
+                        _nodes[nextIndex].HeapIndex = indexSave;
+
+                        if (hasBeenLookedAt)
+                        {
+                            _heap.SortUpNode(nextIndex);
+                        }
+                        else
+                        {
+                            _heap.Add(nextIndex);
+                        }
+                    }
+
+                    if (nextIndex == _end)
+                        return _end;
+                }
+            }
+
+            return nodeIndex;
+        }
+
+        private int Distance(int x1, int y1, int x2, int y2)
+        {
+            int xDiff = Math.Abs(x2 - x1);
+            int yDiff = Math.Abs(y2 - y1);
+
+            int high = Math.Max(xDiff, yDiff);
+            int low = Math.Min(xDiff, yDiff);
+
+            return
+                DiagonalCost * low +
+                StraightCost * (high - low);
+        }
+
+        private List<Coord> GetSearchedPath(NodeIndex mazeStart, NodeIndex from)
+        {
+            Coord toCoord(NodeIndex index) => new Coord(index % Width, index / Width);
+
+            List<Coord> path = new List<Coord>(
+                Distance(_startX, _startY, _endX, _endY) / 10
+                ) { toCoord(from) };
+
+            while ((from = _nodes[from].Parent) != mazeStart && path.Count <= Width * Height)
+                path.Add(toCoord(from));
+
+            path.Reverse();
+
+            return path;
+        }
+
+        private bool IsOutside(int x, int y)
+        {
+            return
+                x < 0 || x >= Width ||
+                y < 0 || y >= Height;
+        }
+
+        public void SetCellWalkable(int x, int y, bool v)
+        {
+            Map[y, x] = !v;
+        }
+    }
+}
---
+++ Assets/PathfinderDebugger.cs
@@ -0,0 +1,98 @@
+using AStarPathfindingMaze;
+using System.Collections.Generic;
+using Timer = System.Diagnostics.Stopwatch;
+using System.Linq;
+using UnityEngine;
+using System;
+using UnityEngine.Tilemaps;
+
+public class PathfinderDebugger : MonoBehaviour
+{
+    public int Size;
+    public GameObject PathStart;
+    public GameObject PathEnd;
+    public PathFinder Pathfinder;
+    public bool On;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PathStart = GameObject.Find("PathStart");
+        PathEnd = GameObject.Find("PathEnd");
+        Pathfinder = new PathFinder(Size, Size);
+    }
+
+    // AI GEN
+    void UpdatePathFinderMap()
+    {
+        // Find the "Nonwalkable" tilemap
+        var nonWalkableGO = GameObject.Find("Nonwalkable");
+        if (nonWalkableGO == null)
+        {
+            Debug.LogError("No GameObject named 'Nonwalkable' found!");
+            return;
+        }
+
+        var nonWalkableTilemap = nonWalkableGO.GetComponent<Tilemap>();
+        if (nonWalkableTilemap == null)
+        {
+            Debug.LogError("'Nonwalkable' GameObject has no Tilemap component!");
+            return;
+        }
+
+        // Clear existing map
+        Array.Clear(Pathfinder.Map, 0, Pathfinder.Map.Length);
+
+        // Get the bounds of the tilemap
+        BoundsInt bounds = nonWalkableTilemap.cellBounds;
+
+        // Iterate through all cells in the tilemap
+        foreach (var pos in bounds.allPositionsWithin)
+        {
+            TileBase tile = nonWalkableTilemap.GetTile(pos);
+            if (tile != null)
+            {
+                // Convert tilemap position to your pathfinder grid coords if needed
+                int x = pos.x;
+                int y = pos.y;
+
+                Pathfinder.SetCellWalkable(x + (Size/2), y + (Size / 2), false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fill the pathfinder grid with all the objects in the Grid->Nonwalkable tilemap
+        UpdatePathFinderMap();
+
+        Vector2 start = PathStart.transform.position;
+        Vector2 end = PathEnd.transform.position;
+
+        Vector2 halfSize = new Vector2(Size / 2f, Size / 2f);
+
+        // Transform
+        start += halfSize;
+        end += halfSize;
+
+        Timer sw = Timer.StartNew();
+        List<Coord> path = Pathfinder.SearchPath(
+            start: new Coord() { x = (int)Mathf.Floor(start.x), y = (int)Mathf.Floor(start.y) },
+            end: new Coord() { x = (int)Mathf.Floor(end.x), y = (int)Mathf.Floor(end.y) },
+            doSmoothingParse: false
+        );
+        double timeMs = sw.Elapsed.TotalMilliseconds;
+        Debug.Log("Pathfinder timer: " + timeMs + " ms, Path length: " + path.Count);
+
+        List<Vector2> vPath = path.Select(c => new Vector2(c.x, c.y) - halfSize).ToList();
+
+        // Draw path
+        Vector2 offset = new Vector2(0.5f, 0.5f);
+        Debug.DrawLine(start - halfSize, vPath[0] + offset, Color.red);
+        for (int i = 0; i < vPath.Count - 1; i++)
+        {
+            Debug.DrawLine(vPath[i] + offset, vPath[i + 1] + offset, Color.red);
+        }
+    }
+}
