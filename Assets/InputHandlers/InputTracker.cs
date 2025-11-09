cc4e4ef55c929be4bf8d5920cd0456a6e1c154ad
CeniusM <cenius.meier@gmail.com>
2025-11-09 17:14:25 +0100

    IT MIGHT JUST WORK!! I think it does for the keyboard now :D

--- Assets/InputHandlers/InputTracker.cs
+++ Assets/InputHandlers/InputTracker.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
 
 public enum UserInputType
 {
-    Keypress,
+    KeyChange,
     MouseClick,
     //MouseMovement
 }
@@ -16,57 +19,38 @@ public class UserInput
     public UserInputType InputType;
     public double TimeStamp;
 
-    public KeyCode KeyCode;
+    // Keyboard
+    public Key KeyCode;
+    public bool WasPressed;
+
+    // Mouse
     public Vector2 MouseClickPosition;
     //public int
+
+    public override string ToString()
+    {
+        return $"UserInput: {InputType} at {TimeStamp}";
+    }
 }
 
 // We get all the inputs so that the game logic can run through all the events since the last fixedframe such that we dont drop any inputs
 // When the next fixedframe starts, the game logic can get the current history and let a new one accumulate for next fixedframe
 public class InputTracker : MonoBehaviour
 {
+    // A collection of the userinputs from last frame. The history sorted from 0->first to N->last
+    public UserInput[] LastFrameInputsHistory;
+
     // Accumulates each frame for game logic to use on each fixed update
-    public List<UserInput> InputQueue;
+    private List<UserInput> _inputList;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InputQueue = new List<UserInput>();
+        LastFrameInputsHistory = Array.Empty<UserInput>();
 
-        var a = InputSystem.actions;
-
-        //a.FindAction("Attack").performed += InputTracker_performed;
-
-        //InputAction action = new InputAction();
-
-        Debug.Log(InputActionMap.ToJson(InputSystem.actions.actionMaps));
-
+        _inputList = new List<UserInput>();
 
         InputSystem.onEvent.ForDevice(Keyboard.current).Call(HandleKeyboardRawEvent);
-
-
-
-    }
-    // Remove inputsystem onevent listiner when program closes
-    private void OnApplicationQuit()
-    {
-        //InputSystem.onEvent.
-    }
-
-
-    public void HandleKeyboardRawEvent(InputEventPtr eventPtr)
-    {
-        //var keyboardState = Keyboard.current.ReadValueFromEventAsObject(eventPtr);
-        //Debug.Log(keyboardState);
-
-        foreach (var a in eventPtr.GetAllButtonPresses())
-        {
-            Debug.Log(a.displayName);
-        }
-        //eventPtr.GetAllButtonPresses());
-        
-        //var keyState = Keyboard.current.pKey.ReadValueFromEvent(eventPtr);
-        //Debug.Log(keyState);
     }
 
     // Update is called once per frame
@@ -75,151 +59,82 @@ public class InputTracker : MonoBehaviour
 
     }
 
-    private void InitializDefaultActions()
+    // This will run first, so all the other GameObjects can use the UserInputHistory
+    void FixedUpdate()
     {
-        var gridBinds = new InputActionMap("GridBinds");
+        // Store the new input history sorted so the new ones can be added and shown next frame
 
+        lock (this)
+        {
+            //if (_inputList.Count > 3)
+            //    Debug.Log("");
 
+            LastFrameInputsHistory = _inputList.ToArray();
+
+            // Hope this is right :)
+            Array.Sort(LastFrameInputsHistory, (x, y) => x.TimeStamp > y.TimeStamp ? 0 : 1);
+
+            _inputList.Clear();
+        }
+    }
+
+    // Remove inputsystem onevent listiner when program closes
+    private void OnApplicationQuit()
+    {
+        //InputSystem.onEvent.
+    }
+
+    public void AddUserInputEvent(UserInput evt)
+    {
+        lock (this)
+        {
+            _inputList.Add(evt);
+        }
+    }
+
+    private void HandleKeyboardRawEvent(InputEventPtr eventPtr)
+    {
+        if (!eventPtr.valid)
+        {
+            Debug.LogWarning("Some invalid keyboard event was given");
+            return;
+        }
+
+        if (eventPtr.type != new FourCC("STAT"))
+            return;
+
+        int changesCount = 0;
+        double timeStamp = eventPtr.time;
+
+        foreach (var change in eventPtr.EnumerateChangedControls())
+        {
+            KeyControl keyChange = change is KeyControl ? (KeyControl)change : null;
+
+            if (keyChange == null)
+            {
+                Debug.LogWarning("There was a keyboard change that did not involve a keycontrol");
+                continue;
+            }
+
+            bool isPressed = keyChange.magnitude == 0; // 0 = down, 1 = up
+            Key kCode = keyChange.keyCode;
+
+            //string keyControlName = keyChange.name;
+            //string keyControlDisblayName = keyChange.displayName;
+            //Debug.Log($"isPressed: {isPressed} --- kCode: {kCode} --- keyControlName: {keyControlName} --- keyControlDisblayName: {keyControlDisblayName} ");
+
+            AddUserInputEvent(new UserInput()
+            {
+                InputType = UserInputType.KeyChange,
+                TimeStamp = timeStamp,
+                KeyCode = kCode,
+                WasPressed = isPressed
+            });
+
+            changesCount++;
+        }
+
+        if (changesCount > 1)
+            Debug.LogWarning("There was somehow more than 1 event during the same timestamp");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.InputSystem;
-//using UnityEngine.InputSystem.LowLevel;
-
-//public enum UserInputType
-//{
-//    Keypress,
-//    MouseClick,
-//    //MouseMovement
-//}
-
-//public class UserInput
-//{
-//    public UserInputType InputType;
-//    public double TimeStamp;
-
-//    public KeyCode KeyCode;
-//    public Vector2 MouseClickPosition;
-//    //public int
-//}
-
-//// We get all the inputs so that the game logic can run through all the events since the last fixedframe such that we dont drop any inputs
-//// When the next fixedframe starts, the game logic can get the current history and let a new one accumulate for next fixedframe
-//public class InputTracker : MonoBehaviour
-//{
-//    public List<UserInput> InputQueue;
-//    public GameObject InputTrackerObj; // Contains the Player Input Actions
-
-
-//    public void KeyInputChange(InputAction.CallbackContext context)
-//    {
-//        //InputSystem.
-
-//        //InputSettings.UpdateMode.ProcessEventsManually
-//        //if (!context.performed)
-//        //    Debug.Log("Was not performed?");
-//        //if (context.performed)
-//        //{
-//        //    Debug.Log("Something happend: " + context.time);
-//        //}
-//        //Debug.Log(context.)
-//        //Debug.Log(context);
-//        //Debug.Log(context.valueType);
-//        //Debug.Log(context.control);
-//        //Debug.Log(context.ReadValueAsObject());
-//    }
-
-
-//    InputRecorder recorder = null;
-//    public void SpacebarIGuess(InputAction.CallbackContext context)
-//    {
-//        //if (!context.performed)
-//        //    return;
-
-//        //string filePath = "C:\\Users\\ceniu\\MyRTSGame\\TempLogs\\input_logs.txt";
-
-//        //// Set up a recorder with such that it automatically grows in size as needed.
-//        //if (recorder == null)
-//        //{
-//        //    recorder = new InputRecorder();
-//        //    //recorder.   
-//        //    //recorder.Enable();
-//        //    Debug.Log("Tracing");
-//        //}
-//        //else
-//        //{
-//        //    //recorder.WriteTo(filePath);
-
-
-//        //    recorder.Disable();
-//        //    recorder = null;
-//        //    Debug.Log("Stopped tracing");
-//        //}
-
-//        //// Load trace from same file.
-//        ////var loadedTrace = InputEventTrace.LoadFrom("mytrace.inputtrace");
-//    }
-
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Start()
-//    {
-//        //History = new List<UserInput>();
-//        //InputSystem.onActionChange += InputSystem_onActionChange;
-
-//        InputTrackerObj = GameObject.Find("InputTracker");
-
-//        var a = InputSystem.actions;
-
-//        a.FindAction("Attack").performed += InputTracker_performed;
-
-//        InputAction action = new InputAction();
-
-//        //Debug.Log(InputActionMap.ToJson(InputSystem.actions.actionMaps));
-
-//    }
-
-//    private void InputTracker_performed(InputAction.CallbackContext obj)
-//    {
-//        Debug.Log("Somebody attacked");
-//    }
-
-//    private void InputSystem_onActionChange(object obj, InputActionChange change)
-//    {
-//        // obj can be either an InputAction or an InputActionMap
-//        // depending on the specific change.
-
-//        switch (change)
-//        {
-//            case InputActionChange.ActionStarted:
-//            case InputActionChange.ActionPerformed:
-//            case InputActionChange.ActionCanceled:
-//                Debug.Log($"{((InputAction)obj)} {change}");
-//                break;
-//        }
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-//}
