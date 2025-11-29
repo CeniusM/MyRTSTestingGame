90e03fa8dd3c8ba3270e2fbf9ce6ab320c15ae63
CeniusM <cenius.meier@gmail.com>
2025-11-29 19:22:13 +0100

    Working on input system

--- Assets/GameLogic/UserInputInterpreter.cs
+++ Assets/GameLogic/UserInputInterpreter.cs
@@ -1,2 +1,31 @@
 // Could use a class like this to translate the user inputs to something the game world could use
 // But for now the WorldManager takes care of it all
+using UnityEngine;
+
+public class UserInputInterpreter
+{
+    WorldManager worldManager;
+    PlayerData playerData;
+    FrameInputTracker inputTracker;
+
+    public UserInputInterpreter(WorldManager worldManager, PlayerData playerData, FrameInputTracker inputTracker)
+    {
+        this.inputTracker = inputTracker;
+        this.worldManager = worldManager;
+        this.playerData = playerData;
+    }
+
+    public Vector2 dragStartPos = new Vector2();
+    public bool isDragging = false;
+
+    // Called at the start of each FixedUpdate in WorldManager
+    public void InterpretInput()
+    {
+        var inputs = inputTracker.LastFrameInputsHistory;
+
+        foreach (var input in inputs)
+        {
+
+        }
+    }
+}
--- Assets/GameLogic/WorldManager.cs
+++ Assets/GameLogic/WorldManager.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 // This is where the main logic of the game comes from
 public class WorldManager : MonoBehaviour
 {
+    public LayerMask UnitLayer;
+    public LayerMask GroundLayer;
+
     private FrameInputTracker _inputTracker;
+    private UserInputInterpreter _inputInterpreter;
+    private PlayerData _playerData;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,25 +23,85 @@ public class WorldManager : MonoBehaviour
         {
             Debug.Log("WorldManager connected to InputTracker.");
         }
+
+        _playerData = FindAnyObjectByType<PlayerData>();
+
+        if (_playerData == null)
+        {
+            Debug.LogError("PlayerData not found in the scene!");
+        }
+        else
+        {
+            Debug.Log("WorldManager connected to PlayerData.");
+        }
+
+        _inputInterpreter = new UserInputInterpreter(this, _playerData, _inputTracker);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        // Test UserInputs
-        if (_inputTracker == null || _inputTracker.LastFrameInputsHistory == null)
-            return;
+        _inputInterpreter.InterpretInput();
+    }
 
-        var inputs = _inputTracker.LastFrameInputsHistory;
-
-        if (inputs.Length > 0)
+    // Maybe move to other class later
+    public LayerMask unitLayer;
+    public LayerMask groundLayer;
+    public Color selectionBoxColor = new Color(0, 1, 0, 0.2f); // translucent green
+    void OnGUI()
+    {
+        Rect GetScreenRect(Vector2 screenPosition1, Vector2 screenPosition2)
         {
-            Debug.Log("Frame Input Events:");
-            foreach (var input in inputs)
-            {
-                //if (input.InputType == UserInputType.MouseMovement)
-                Debug.Log(" * " + input);
-            }
+            // Move origin from bottom left to top left
+            screenPosition1.y = Screen.height - screenPosition1.y;
+            screenPosition2.y = Screen.height - screenPosition2.y;
+            var topLeft = Vector2.Min(screenPosition1, screenPosition2);
+            var bottomRight = Vector2.Max(screenPosition1, screenPosition2);
+            return Rect.MinMaxRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
+        }
+
+        void DrawScreenRect(Rect rect, Color color)
+        {
+            GUI.color = color;
+            GUI.DrawTexture(rect, Texture2D.whiteTexture);
+            GUI.color = Color.white;
+        }
+
+        void DrawScreenRectBorder(Rect rect, float thickness, Color color)
+        {
+            // Top
+            DrawScreenRect(new Rect(rect.xMin, rect.yMin, rect.width, thickness), color);
+            // Left
+            DrawScreenRect(new Rect(rect.xMin, rect.yMin, thickness, rect.height), color);
+            // Right
+            DrawScreenRect(new Rect(rect.xMax - thickness, rect.yMin, thickness, rect.height), color);
+            // Bottom
+            DrawScreenRect(new Rect(rect.xMin, rect.yMax - thickness, rect.width, thickness), color);
+        }
+
+        if (_inputInterpreter.isDragging)
+        {
+            var rect = GetScreenRect(_inputInterpreter.dragStartPos, Input.mousePosition);
+            DrawScreenRect(rect, selectionBoxColor);
+            DrawScreenRectBorder(rect, 2, Color.green);
         }
     }
 }
+
+
+
+
+//// Test UserInputs
+//if (_inputTracker == null || _inputTracker.LastFrameInputsHistory == null)
+//    return;
+
+//var inputs = _inputTracker.LastFrameInputsHistory;
+
+////if (inputs.Length > 0)
+////{
+////    Debug.Log("Frame Input Events:");
+////    foreach (var input in inputs)
+////    {
+////        //if (input.InputType == UserInputType.MouseMovement)
+////        Debug.Log(" * " + input);
+////    }
+////}
